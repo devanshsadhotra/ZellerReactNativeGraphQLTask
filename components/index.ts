98842a244c98689dd8ef48174ac24f4674044a1e
Devansh <devanshsadhotra@outlook.com>
2023-10-07 15:51:40 +0530

    removing unused components

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
-import ListView from './listView';
 import Container from './container';
 import RadioButton from './radioButton';
 import SearchInput from './searchInput';
 
-export {ListView, RadioButton, SearchInput, Container};
+export { RadioButton, SearchInput, Container};
