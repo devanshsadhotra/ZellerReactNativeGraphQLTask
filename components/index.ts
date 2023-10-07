5badf907a5efa8f6cc82ef8886c7ab3e6fcd4181
Devansh <devanshsadhotra@outlook.com>
2023-10-07 15:52:58 +0530

    more unused components removed

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
-import Container from './container';
 import RadioButton from './radioButton';
 import SearchInput from './searchInput';
 
-export { RadioButton, SearchInput, Container};
+export { RadioButton, SearchInput};
